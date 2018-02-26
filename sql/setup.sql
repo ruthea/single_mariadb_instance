8239649d6a2e0c51ecacea8750f910c9563f13ce
Todd Stoffel <toddstoffel@icloud.com>
2018-02-26 18:15:36 -0500

    updates

--- sql/setup.sql
+++ sql/setup.sql
@@ -3,6 +3,7 @@ INSTALL SONAME 'ha_oqgraph';
 INSTALL SONAME 'ha_connect';
 INSTALL SONAME 'ha_rocksdb';
 INSTALL SONAME 'auth_pam';
+
 CREATE ROLE IF NOT EXISTS 'admin';
 GRANT ALL PRIVILEGES ON *.* TO 'admin' WITH GRANT OPTION;
 CREATE USER IF NOT EXISTS 'vagrant'@'%' IDENTIFIED VIA pam USING 'mariadb';
@@ -11,4 +12,5 @@ GRANT 'admin' TO 'vagrant'@'%';
 GRANT 'admin' to 'vagrant'@'localhost';
 SET DEFAULT ROLE 'admin' FOR 'vagrant'@'%';
 SET DEFAULT ROLE 'admin' FOR 'vagrant'@'localhost';
+
 FLUSH PRIVILEGES;
